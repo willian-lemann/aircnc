f22e170446ede443c36c7d3c54671606d86bd16c
willian <willianleman@hotmail.com>
2019-10-09 00:41:50 -0300

    inicio da aplicacao mobile

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+
+export default function List() {
+    return (
+        <View>
+            <Text>Hello Willian</Text>
+        </View>
+    );
+}
